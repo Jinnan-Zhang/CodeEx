1ed9383dedf3f0f98b677f20fa9548a39c982bdd
JinnanZhang <zhangjinnan14@ucas.edu.cn>
2019-11-18 19:46:49 +0800

    source

--- Roottest/h2test.cc
+++ Roottest/h2test.cc
@@ -8,7 +8,7 @@ int h2test(){
     TF2 *f2=new TF2("f2","x*x+y*y",-10,10,-10,10);
     TH2D *h1=new  TH2D("h2","",200,-1,1,200,-1,1);
     h1->FillRandom("f2",1e4);
-    //h1->Draw("surf");
+    //h1->Draw("surf");hist/draw2dopt.C
     f2->Draw("surf3");
     return  0;
 }
