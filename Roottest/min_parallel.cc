78a054d5f7db6e5a4d78a03f36a5429abfa24494
JinnanZhang <zhangjinnan14@ucas.edu.cn>
2019-11-18 22:58:13 +0800

    On parallel

---
+++ Roottest/min_parallel.cc
@@ -0,0 +1,99 @@
+#include "ROOT/TSeq.hxx"
+#include "ROOT/TThreadExecutor.hxx"
+#include "PoolUtils.h"
+
+#include <TMath.h>
+#include "Math/Minimizer.h"
+#include "Math/Factory.h"
+#include "Math/Functor.h"
+#include "TRandom3.h"
+#include "TError.h"
+
+#include <TCanvas.h>
+#include <TH1.h>
+#include <iostream>
+using namespace std;
+
+const UInt_t poolSize = 4U;
+double x_0, y_0, z_0;
+double wrapx0(const double *x)
+{
+    double y = x[0];
+    double z = x[1];
+    return 10 * x_0 * x_0 + y * y + z * z*z*z;
+}
+double wrapx1(const double *x)
+{
+    double x1 = x[0];
+    double z = x[1];
+    return 10 * x1 * x1 + y_0 * y_0 + z * z*z*z;
+}
+
+double min_x(double x_v, int par)
+{
+    ROOT::Math::Minimizer *miniChi = ROOT::Math::Factory::CreateMinimizer("Minuit2", "Migrad");
+    //set tolerance , etc...
+    miniChi->SetMaxFunctionCalls(100000); // for Minuit/Minuit2
+    miniChi->SetMaxIterations(1000);      // for GSL
+    miniChi->SetTolerance(0.001);
+    miniChi->SetPrintLevel(4);
+    //create function wrapper for minimizer
+    //a IMultiGenFunction type
+    int SN4par = 0;
+
+    switch (par)
+    {
+    case 0: //
+    {
+        x_0 = x_v;
+        ROOT::Math::Functor fG(&wrapx0, 2);
+        miniChi->SetFunction(fG);
+        double steps = 0.01;
+        double starts = 0;
+        miniChi->SetVariable(SN4par++, "y", starts, steps);
+        miniChi->SetVariable(SN4par++, "z", starts, steps);
+        miniChi->Minimize();
+        return miniChi->MinValue();
+    }
+    case 1:
+    {
+        y_0 = x_v;
+        ROOT::Math::Functor fG(&wrapx1, 2);
+        miniChi->SetFunction(fG);
+        double steps = 0.01;
+        double starts = 0;
+        miniChi->SetVariable(SN4par++, "x", starts, steps);
+        miniChi->SetVariable(SN4par++, "z", starts, steps);
+        miniChi->Minimize();
+        return miniChi->MinValue();
+    }
+    }
+    return 0;
+}
+
+int min_parallel()
+{
+
+    ROOT::EnableThreadSafety();
+    TH1::AddDirectory(false);
+    ROOT::TThreadExecutor pool(poolSize);
+    auto fillRandomHisto = [](int seed = 0) {
+        auto h = new TH1F("myHist", "Filled in parallel", 40, -8, 8);
+        for (auto i : ROOT::TSeqI(200))
+        {
+            double aa = -8. + i * 0.4 / 200.;
+            h->Fill(aa, min_x(aa, 0));
+        }
+        return h;
+    };
+
+    auto seeds = ROOT::TSeqI(1);
+    ROOT::ExecutorUtils::ReduceObjects<TH1F *> redfunc;
+    auto sumRandomHisto = pool.MapReduce(fillRandomHisto, seeds, redfunc);
+
+    auto c = new TCanvas();
+    sumRandomHisto->Draw();
+
+    
+    return 0;
+}
