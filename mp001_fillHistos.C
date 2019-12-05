e0ddff3d2f3e32172565ddf0373892757387352c
Jinnan Zhang <zhangjn@ihep.ac.cn>
2019-12-05 16:11:56 +0800

    parallel read good

--- mp001_fillHistos.C
+++ mp001_fillHistos.C
@@ -8,7 +8,7 @@
 using namespace std;
 
 // The number of workers
-const UInt_t nWorkers = 4U;
+const UInt_t nWorkers = 6U;
 int doit(UInt_t workerID);
 static TH1D *h1 = new TH1D("h1", "", 20, -10, 10);
 double a[10];
@@ -24,15 +24,15 @@ Int_t mp001_fillHistos()
    workers.Map(workItem, ROOT::TSeqI(nWorkers));
 
    // TFile *fx=TFile::Open("myfile.root","RECREATE");
-   h1->Draw();
+   // h1->Draw();
 
    return 0;
 }
 int doit(UInt_t workerID)
 {
-   for (int i = 0; i < 20; i++)
-      h1->SetBinContent(i+1,workerID+i);
-
-   cout << workerID << endl;
+   TFile *f=TFile::Open("a.root","READ");
+   TH1* h2=0;
+   f->GetObject("h1",h2);
+   cout << h2->GetBinContent(50) << endl;
    return 0;
 };
