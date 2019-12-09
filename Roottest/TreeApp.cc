3a55c218084cdd050b01f47388d74be6fab077d7
Jinnan Zhang <zhangjn@ihep.ac.cn>
2019-12-09 16:28:23 +0800

    stupid root

--- Roottest/TreeApp.cc
+++ Roottest/TreeApp.cc
@@ -2,43 +2,45 @@
 #include <iostream>
 #include <TFile.h>
 #include <TRandom3.h>
+#include <string>
+#include <TH2.h>
+#include <TF2.h>
 
 using namespace std;
 int TreeApp()
 {
+    TH1::AddDirectory(false);
     TFile *file1 = TFile::Open("app.root", "UPDATE");
     TTree *t1 = 0;
     if ((TTree *)file1->Get("t1"))
         t1 = (TTree *)file1->Get("t1");
     else
         t1 = new TTree("t1", "app test");
-    // t1 = ((TTree *)file1->Get("t1") ? (TTree *)file1->Get("t1") : new TTree("t1", "app test"));
-    double sinsq;
-    double dmsq;
-    if (t1->SetBranchAddress("sinsq", &sinsq) > 0)
+    struct COM
     {
-        t1->SetBranchAddress("sinsq", &sinsq);
+        double sinsq;
+        double dmsq;
+    } Coo;
+
+    string anm = "try";
+    if (t1->SetBranchAddress(anm.c_str(), &Coo) > 0)
+    {
+        t1->SetBranchAddress(anm.c_str(), &Coo);
     }
     else
     {
-        t1->Branch("sinsq", &sinsq, "sinsq/D");
-    }
-    if (t1->SetBranchAddress("dmsq", &dmsq) > 0)
-    {
-        t1->SetBranchAddress("dmsq", &dmsq);
-    }
-    else
-    {
-        t1->Branch("dmsq", &dmsq, "dmsq/D");
+        t1->Branch(anm.c_str(), &Coo, "sinsq/D:dmsq/D");
     }
 
-    for (int i = 0; i < 3e4; i++)
+    for (int i = 0; i < 3e3; i++)
     {
-        dmsq = gRandom->Gaus(i,i);
-        sinsq = gRandom->Gaus(i,i);
+        // double a=gRandom->Gaus(0, 10);
+        Coo.dmsq = gRandom->Gaus(0, 10);
+        Coo.sinsq =  gRandom->Gaus(0, 10);
         t1->Fill();
     }
-    t1->Write("",TObject::kOverwrite);
+    t1->Fill();
+    t1->Write("", TObject::kOverwrite);
     file1->Close();
     return 0;
 }
