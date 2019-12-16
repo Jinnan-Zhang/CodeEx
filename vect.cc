2b770f609c0caddc27ef0f4910287a9a73d20d49
Jinnan Zhang <zhangjn@ihep.ac.cn>
2019-12-16 15:41:10 +0800

    cal error

--- vect.cc
+++ vect.cc
@@ -1,20 +1,33 @@
 #include <map>
-#include<vector>
-#include<iostream>
+#include <vector>
+#include <iostream>
+#include <cmath>
 
 using namespace std;
-void avec(){
-    vector<double> aa;
-    for(int i=0;i<10;i++)
+void avec()
+{
+    double BkgRatio[5];
+    BkgRatio[0] = 0.9 / 60;
+    //Fast neutron bkg
+    BkgRatio[1] = 0.1 / 60;
+    //9Li8He bkg
+    BkgRatio[2] = 1.6 / 60;
+    //alpha n bkg
+    BkgRatio[3] = 0.05 / 60;
+    //Geo-nus bkg
+    BkgRatio[4] = 1.1 / 60;
+    vector<double> sigma1 = {0.01, 1, 0.2, 0.5, 0.3};
+    vector<double> sigma = {0, 0.2, 0.1, 0.5, 0.05};
+    double give = 0;
+    for (int i = 0; i < 5; i++)
     {
-        aa.push_back(i);
+        give += BkgRatio[i] * sigma[i]*BkgRatio[i] * sigma[i];
     }
-    cout<<"size: "<<aa.size()<<endl;
+    cout <<sqrt(give)<<endl;
 }
 int vect()
-{  
+{
     avec();
-    avec();
+    // avec();
     return 0;
-    
 }
