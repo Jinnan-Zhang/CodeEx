f9c66151dd85daa9240b691e84700c63e57d9f1e
Jinnan <zhangjinnan@ihep.ac.cn>
2020-02-20 11:32:11 +0800

    need improve

---
+++ WinShell/CpPMT_input.bat
@@ -0,0 +1,9 @@
+REM @echo off
+cd D:\
+copy /y PMT_Input.txt D:\DCR
+copy /y PMT_Input.txt D:\PDE_setpoint_3000
+copy /y PMT_Input.txt D:\PDE_0,1_PE
+copy /y PMT_Input.txt D:\Afterpulses
+copy /y PMT_Input.txt D:\Meassurements
+copy /y PMT_Input.txt D:\DCR
+pause
---
+++ WinShell/CutData.bat
@@ -0,0 +1,5 @@
+REM @echo off
+cd D:\
+for /d %%a in (gain_Iteration*) do move %%a D:\
+
+pause
