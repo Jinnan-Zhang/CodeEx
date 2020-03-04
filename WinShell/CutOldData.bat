45e9c8e47941048aea3b444e8c53fd603993a4ea
Jinnan <zhangjinnan@ihep.ac.cn>
2020-03-04 16:11:56 +0800

    mkdir

--- WinShell/CutOldData.bat
+++ WinShell/CutOldData.bat
@@ -4,6 +4,7 @@ cd D:\
 echo please enter mass number y/n:
 set /p Nmass=
 set Target=D:\previous measurements1\mass%Nmass%
+md %Target%
 for /d %%a in (gain_Iteration*,DCR,PDE_setpoint_3000,PDE_*_PE,Afterpulses,Meassurements,TTS) do move /-y %%a "%Target%"
 move /-Y PMT_Input.txt "%Target%"
 
