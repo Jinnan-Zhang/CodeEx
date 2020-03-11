a34a36a01bb4899ddb1111b8f1608a375adf3e03
Jinnan Zhang <zhangjinnan@ihep.ac.cn>
2020-03-11 09:48:40 +0800

    TH1 better

--- Roottest/data/ShowNL.cc
+++ Roottest/data/ShowNL.cc
@@ -1,17 +1,27 @@
 #include <TFile.h>
 #include <TGraph.h>
 #include <iostream>
-using std::cout, std::endl;
+#include <TH1.h>
+// using std::cout, std::endl;
+
 
 int ShowNL()
 {
+    TH1::AddDirectory(false);
     TFile *ff = TFile::Open("energymodel_Apr2018_newE.root", "READ");
-    TGraph *g0 = 0;
-    ff->GetObject("positronFullNL", g0);
-    // for(int i=0;i<1e6;i++)
-    // {
-    //     g0->Eval(4);
-    // }
-    g0->Draw();
-     return 0;
+    TFile *ff_h=TFile::Open("histNL.root","READ");
+    TH1D *h1=0;
+    ff_h->GetObject("h1",h1);
+    for(int i=0;i<1e7;i++)
+    {
+        printf("%f\n",h1->Interpolate(4));
+    }
+    // TGraph *g0 = 0;
+    // ff->GetObject("positronFullNL", g0);
+    // // for(int i=0;i<1e6;i++)
+    // // {
+    // //     g0->Eval(4);
+    // // }
+    // g0->Draw();
+    return 0;
 }
