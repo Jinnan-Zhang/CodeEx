9ddc598a35d686bb9a8934864e316e7f0579c753
Jinnan Zhang <zhangjinnan@ihep.ac.cn>
2020-03-12 08:07:03 +0800

    time does not work

--- Roottest/data/ShowNL.cc
+++ Roottest/data/ShowNL.cc
@@ -3,7 +3,7 @@
 #include <iostream>
 #include <TH1.h>
 // using std::cout, std::endl;
-
+#include <timeMacro.h>
 
 int ShowNL()
 {
@@ -12,16 +12,23 @@ int ShowNL()
     TFile *ff_h=TFile::Open("histNL.root","READ");
     TH1D *h1=0;
     ff_h->GetObject("h1",h1);
+    StartTimeChrono(1);
     for(int i=0;i<1e7;i++)
     {
         printf("%f\n",h1->Interpolate(4));
     }
-    // TGraph *g0 = 0;
-    // ff->GetObject("positronFullNL", g0);
-    // // for(int i=0;i<1e6;i++)
-    // // {
-    // //     g0->Eval(4);
-    // // }
+    StopTimeChrono(1);
+
+    TGraph *g0 = 0;
+    ff->GetObject("positronFullNL", g0);
+    StopTimeChrono(2);
+    for(int i=0;i<1e6;i++)
+    {
+        g0->Eval(4);
+    }
+    StopTimeChrono(2);
+    PrintTimeChrono(1,"TH1");
+    PrintTimeChrono(2,"TG");
     // g0->Draw();
     return 0;
 }
---
+++ Roottest/data/timeMacro.h
@@ -0,0 +1,25 @@
+#pragma once
+/*
+usage:
+StartTimeChrono(num);
+...
+StopTimeChrono(num);
+PrintTimeChrono(num,"discription")
+*/
+#include <chrono>
+
+#define StartTimeChrono(_num) \
+  auto start##_num = std::chrono::high_resolution_clock::now()
+
+#define StopTimeChrono(_num) \
+  auto stop##_num = std::chrono::high_resolution_clock::now()
+
+#ifdef _WIN32
+#define PrintTimeChrono(_num, _str)                                                                  \
+  auto dura##_num = std::chrono::duration_cast<std::chrono::microseconds>(stop##_num - start##_num); \
+  printf("Time for calculating %s is %f millisecond.\n", _str, dura##_num.count())
+#else
+#define PrintTimeChrono(_num, _str)                                                                  \
+  auto dura##_num = std::chrono::duration_cast<std::chrono::microseconds>(stop##_num - start##_num); \
+  printf("Time for calculating %s is %f second.\n", _str, dura##_num.count() / 1.e6)
+#endif
