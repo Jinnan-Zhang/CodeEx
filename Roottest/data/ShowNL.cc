1a7c747cb5d6ced8fda7ec27190008cdd6f13786
Jinnan Zhang <zhangjinnan@ihep.ac.cn>
2020-03-12 08:16:06 +0800

    TH1 much better than Tgraph

--- Roottest/data/ShowNL.cc
+++ Roottest/data/ShowNL.cc
@@ -3,7 +3,7 @@
 #include <iostream>
 #include <TH1.h>
 // using std::cout, std::endl;
-#include <timeMacro.h>
+#include <time.h>
 
 int ShowNL()
 {
@@ -12,23 +12,28 @@ int ShowNL()
     TFile *ff_h=TFile::Open("histNL.root","READ");
     TH1D *h1=0;
     ff_h->GetObject("h1",h1);
-    StartTimeChrono(1);
+
+    clock_t start, finish;
+    start=clock();
     for(int i=0;i<1e7;i++)
     {
-        printf("%f\n",h1->Interpolate(4));
+        h1->Interpolate(4);
     }
-    StopTimeChrono(1);
+    finish=clock();
+    printf( "%f seconds\n",(double)(finish - start)/1000.);
+    
 
     TGraph *g0 = 0;
     ff->GetObject("positronFullNL", g0);
-    StopTimeChrono(2);
+    start=clock();
     for(int i=0;i<1e6;i++)
     {
         g0->Eval(4);
     }
-    StopTimeChrono(2);
-    PrintTimeChrono(1,"TH1");
-    PrintTimeChrono(2,"TG");
+    finish=clock();
+    printf( "%f seconds\n",(double)(finish - start)/1000.);
+
+    
     // g0->Draw();
     return 0;
 }
