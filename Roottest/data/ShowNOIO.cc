dd110deda19e4d2003a424adb0ba6964bbb9d7c6
Jinnan Zhang <zhangjinnan@ihep.ac.cn>
2020-03-13 14:19:24 +0800

    MO diff

---
+++ Roottest/data/ShowNOIO.cc
@@ -0,0 +1,46 @@
+//目的:展示能谱差异
+#include<TFile.h>
+#include<TH1.h>
+#include <TLegend.h>
+double E_Lower = 1.807;             //energy threshold of IBD
+double E_Upper = 10;               //energy cut for neutrino energy
+double E_LP = 1.022;                //E_Lower-0.78233108;
+double E_UP = E_Upper - 0.78233108; 
+
+int ShowNOIO()
+{
+    TH1::SetDefaultSumw2();
+    TH1::AddDirectory(false);
+    TFile *ff_NO=TFile::Open("SoCallData_NO.root","READ");
+    TFile *ff_IO=TFile::Open("SoCallData_IO.root","READ");
+    TH1D *h_NO=0,*h_IO=0;
+    ff_NO->cd();
+    ff_NO->GetObject("h_NMO",h_NO);
+    ff_IO->cd();
+    ff_IO->GetObject("h_NMO",h_IO);
+    h_IO->SetLineColor(kBlue);
+    TH1D *h_new=new TH1D("h_ratio","",200,E_LP,E_UP);
+    h_new->Divide(h_IO,h_NO);
+    h_new->SetLineColor(kRed);
+
+    // h_NO->Draw();
+    // h_IO->Draw("SAME");
+    // h_IO->Draw();
+    // h_new->Draw("SAME");
+    h_new->Draw();
+    TLegend leg(5,10);
+    // leg.AddEntry(h_NO,"Normal Ordering");
+    // leg.AddEntry(h_IO,"Invert Ordering");
+    leg.AddEntry(h_new,"IH/NH");
+    leg.Draw("SAME");
+    TFile *ff_r=TFile::Open("NOIOratio.root","RECREATE");
+    ff_r->cd();
+    h_new->Write();
+    h_NO->SetName("h_NO");
+    h_NO->Write();
+    h_IO->SetName("h_IO");
+    h_IO->Write();
+    ff_r->Close();
+    return 0;
+
+}
--- Roottest/data/show3d2dDiff.cc
+++ Roottest/data/show3d2dDiff.cc
@@ -6,17 +6,26 @@
 int show3d2dDiff()
 {
     TH1D *h_old=0;
-    TH1D *h_new=0;
-    TFile *ff_old=TFile::Open("SoCallData_NO_OLD.root","READ");
-    TFile *ff_new=TFile::Open("SoCallData_NO.root","READ");
+    TH1D *h_NLNR=0;
+    TH1D *h_NR=0;
+
+    TFile *ff_old=TFile::Open("SoCallData_NO.root","READ");
     ff_old->GetObject("h_NMO",h_old);
-    ff_new->GetObject("h_NMO",h_new);
-    h_new->SetLineColor(kRed);
-    h_new->Draw();
+    
+    TFile *ff_NLNR=TFile::Open("WithNLNR.root","READ");
+    ff_NLNR->GetObject("h_NMO",h_NLNR);
+    h_NLNR->SetLineColor(kRed);
+    TFile *ff_NR=TFile::Open("WithNR.root","READ");
+    ff_NR->GetObject("h_NMO",h_NR);
+    h_NR->SetLineColor(kBlue);
+
+    h_NLNR->Draw();
     h_old->Draw("SAME");
+    h_NR->Draw("SAME");
     TLegend leg(5,10);
     leg.AddEntry(h_old,"w/o neutron recoil and NL");
-    leg.AddEntry(h_new,"w/ neutron recoil and NL");
+    leg.AddEntry(h_NLNR,"w/ neutron recoil and NL");
+    leg.AddEntry(h_NR,"w/ neutron recoil");
     leg.DrawClone("SAME");
 
     return 0;
