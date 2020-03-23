f86872a85bc6209a445e039edd56d2f84d753105
Jinnan Zhang <zhangjinnan@ihep.ac.cn>
2020-03-23 22:53:59 +0800

    res

--- Roottest/histSolver/EresFea.cc
+++ Roottest/histSolver/EresFea.cc
@@ -6,7 +6,7 @@
 
 using namespace std;
 
-double E_Lower = 1.8043; //energy threshold of IBD
+double E_Lower = 1; //energy threshold of IBD
 double E_Upper = 10.;    //energy cut for neutrino energy
 double simpleEres(double *Es, double *par);
 double ThreeEres(double *Es, double *par);
@@ -15,38 +15,37 @@ double ThreeEres(double *Es, double *par);
 //and one factor resolution model for E_Cal
 int EresFea()
 {
-    TF1 *f0 = new TF1("f0", simpleEres, E_Lower,E_Upper,1);
-    TF1* f1=new TF1("f1",ThreeEres,E_Lower,E_Upper,3);
-    // f0->SetParameter(0,0.03);
-    f1->SetParameters(1,1,1);
+    TF1 *f0 = new TF1("f0", simpleEres, E_Lower, E_Upper, 1);
+    TF1 *f1 = new TF1("f1", ThreeEres, E_Lower, E_Upper, 0);
+    f0->SetParameter(0, 0.03);
+    // f1->SetParameters(1, 1, 1);
     f0->SetLineColor(kRed);
-    f1->SetLineColor(kBlue);
-     TCanvas *c1= new TCanvas("c1","multipads",900,700);
-    c1->Divide(2,1);
-    c1->cd(1);
-    f0->Draw();
-    c1->cd(2);
+    // f1->SetLineColor(kBlue);
+    // TCanvas *c1 = new TCanvas("c1", "multipads", 900, 700);
+    // c1->Divide(2, 1);
+    // c1->cd(1);
+    // f0->Draw();
+    // c1->cd(2);
     f1->Draw();
-    c1->Draw();
+    // c1->Draw();
     return 0;
 }
 
 double simpleEres(double *Es, double *par)
 {
     double sigma;
-    double res ;
+    double res;
     // = par[0];
-    res= sqrt(Es[0]);
-    return res;
+    res = sqrt(Es[0]);
+    return par[0];
 }
 double ThreeEres(double *Es, double *par)
 {
-    double sigma;
-    double a = par[0];
-    double b = par[1];
-    double c = par[2];
-    sigma = Es[0] * sqrt(pow(a / sqrt(Es[0]), 2) +
-                         pow(b / Es[0], 2) +
-                         c * c);
-    return sigma;
+    double E_vis=Es[0];
+    double a_res = 3.;  //stochastic term a/sqrt(E)
+    double b_res = 0.7; //constant term b
+    double c_res = 0;    //noise term c/E
+    return sqrt(a_res * a_res / E_vis + b_res * b_res + c_res * c_res / (E_vis * E_vis));
 }
+
+//目的：查看能量分辨函数的特征
