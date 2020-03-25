f29682bb63163368efb58ab52d2cfe247a971f4d
Jinnan Zhang <zhangjinnan@ihep.ac.cn>
2020-03-25 13:51:24 +0800

    need it self

---
+++ Roottest/histSolver/histPar.cc
@@ -0,0 +1,21 @@
+//目标：指针传参会被改变吗
+#include <TH1.h>
+#include <iostream>
+
+using namespace std;
+void tfunc(TH1 *);
+
+int histPar()
+{
+    TH1 *h0;
+    tfunc(h0);
+    h0->Draw();
+    return 0;
+}
+
+void tfunc(TH1 *ht)
+{
+    TH1D *h00 = new TH1D("h00", "", 200, 0, 10);
+    // ht->Copy(h00);
+    ht->FillRandom("gaus");
+}
