4d4b373aa3ae6e28de2e9397137679f725561093
ZHANG JINNAN <zhangjn@lxslc702.ihep.ac.cn>
2020-03-28 07:57:33 +0800

    new d

--- Roottest/EnergyLeakage/ELout.cc
+++ Roottest/EnergyLeakage/ELout.cc
@@ -67,14 +67,14 @@ int ELout()
     // h_el->SetYTitle("Visible Energy(nPhotons/1200)");
     // h_el->SetYTitle("Deposited Energy(MeV)");
     // h_vis->SetXTitle("E (MeV)");
-    TH1D *h_ra = new TH1D("ratio", "", 200, 0.1, 1.001);
+    TH1D *h_ra = new TH1D("ratio", "", 200, 0.5, 1.);
     double E_ratio(0);
     for (int i = 0; i < tE_vis.GetEntries(); i++)
     {
         tE_vis.GetEntry(i);
         tE_true.GetEntry(i);
         E_true = TMath::Sqrt(Px[0] * Px[0] + Py[0] * Py[0] + Pz[0] * Pz[0] + M_electron_sq) + M_e;
-        if (E_true < 4.5 && E_true > 3.5 && E_dep <= E_true)
+        if (E_true < 4.5 && E_true > 3.5 && E_dep < E_true)
         {
             E_ratio = E_dep / E_true;
             h_ra->Fill(E_ratio, 1);
