edb484e74953cca698fc63080581055e385d2c71
Jinnan Zhang <zhangjinnan@ihep.ac.cn>
2020-03-30 22:37:50 +0800

    fit

--- Roottest/EnergyLeakage/FitEL.cc
+++ Roottest/EnergyLeakage/FitEL.cc
@@ -1,13 +1,29 @@
 #include <TFile.h>
 #include <TH1.h>
 #include <TF1.h>
+#include <TLegend.h>
+#include <iostream>
+using namespace std;
 
-TF1 *f_EL=new TF1("f_EL","[0]+[1]*TMath::Exp([2]+[3]*x)+[4]*x",0,2);
+TF1 *f_EL = new TF1("f_EL", "[0]+[1]*TMath::Exp([2]*x*x+[3]*x+[4])", 0, 1);
 
 int FitEL()
 {
-    TFile *ff_EL=TFile::Open("JUNOEnergyLeakage.root","READ");
-    TH1 *h_EL=(TH1*)ff_EL->Get("Eratio");
-    f_EL->SetParameters(0,);
+    TFile *ff_EL = TFile::Open("JUNOEnergyLeakage.root", "READ");
+    TH1 *h_EL = (TH1 *)ff_EL->Get("Eratio");
+    f_EL->SetParameters(0,1,3.95,-1.17,-5.6);
+    TH1D *h_dat=new TH1D("data","",390,0.1,0.998);
+    for (int i = 0; i < h_dat->GetNbinsX(); i++)
+    {
+        // cout << h_EL->GetBinCenter(i + 1) << "  " << h_EL->GetBinContent(i + 1) << endl;
+        h_dat->SetBinContent(i+1,h_EL->GetBinContent(i+1));
+    }
+    h_dat->SetLineColor(kRed);
+    h_dat->Fit(f_EL);
+    TLegend leg0(1,0.5);
+    leg0.AddEntry(f_EL,"Fit function");
+    leg0.AddEntry(h_dat,"Data");
+    leg0.DrawClone("SAME");
 
+    return 0;
 }
