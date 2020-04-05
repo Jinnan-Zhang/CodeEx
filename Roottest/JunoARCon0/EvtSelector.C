acb34048c3c9d69c8ec15e61400e9b59c09ca76d
Jinnan Zhang <zhangjinnan@ihep.ac.cn>
2020-04-05 11:11:06 +0800

    pointer

--- Roottest/JunoARCon0/EvtSelector.C
+++ Roottest/JunoARCon0/EvtSelector.C
@@ -27,6 +27,7 @@
 #include <TH2.h>
 #include <TStyle.h>
 #include <iostream>
+using std::cout, std::endl;
 
 void EvtSelector::Begin(TTree * /*tree*/)
 {
@@ -65,7 +66,8 @@ Bool_t EvtSelector::Process(Long64_t entry)
    // The return value is currently not used.
 
    fReader.SetLocalEntry(entry);
-   printf("nPhotons: %d", nPhotons);
+   printf("nPhotons: %d", *nPhotons);
+   // cout << nPhotons << endl;
 
    return kTRUE;
 }
