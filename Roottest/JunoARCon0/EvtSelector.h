b377b858dbb74b148c0ac0f49818aa13be46ab59
Jinnan Zhang <zhangjinnan@ihep.ac.cn>
2020-04-05 22:39:54 +0800

    far

--- Roottest/JunoARCon0/EvtSelector.h
+++ Roottest/JunoARCon0/EvtSelector.h
@@ -14,14 +14,15 @@
 #include <TTreeReader.h>
 #include <TTreeReaderValue.h>
 #include <TTreeReaderArray.h>
+#include <TH2.h>
 
 // Headers needed by this particular selector
 
-
-class EvtSelector : public TSelector {
-public :
-   TTreeReader     fReader;  //!the tree reader
-   TTree          *fChain = 0;   //!pointer to the analyzed TTree or TChain
+class EvtSelector : public TSelector
+{
+public:
+   TTreeReader fReader; //!the tree reader
+   TTree *fChain = 0;   //!pointer to the analyzed TTree or TChain
 
    // Readers to access the data (delete the ones you do not need).
    // TTreeReaderValue<Int_t> evtID = {fReader, "evtID"};
@@ -55,26 +56,28 @@ public :
    // TTreeReaderArray<Float_t> BoundaryPosX = {fReader, "BoundaryPosX"};
    // TTreeReaderArray<Float_t> BoundaryPosY = {fReader, "BoundaryPosY"};
    // TTreeReaderArray<Float_t> BoundaryPosZ = {fReader, "BoundaryPosZ"};
+   TH2F *h2d;
+   
 
+   EvtSelector(TTree * /*tree*/ = 0)
+   {
+   }
+   virtual ~EvtSelector() {}
+   virtual Int_t Version() const { return 2; }
+   virtual void Begin(TTree *tree);
+   virtual void SlaveBegin(TTree *tree);
+   virtual void Init(TTree *tree);
+   virtual Bool_t Notify();
+   virtual Bool_t Process(Long64_t entry);
+   virtual Int_t GetEntry(Long64_t entry, Int_t getall = 0) { return fChain ? fChain->GetTree()->GetEntry(entry, getall) : 0; }
+   virtual void SetOption(const char *option) { fOption = option; }
+   virtual void SetObject(TObject *obj) { fObject = obj; }
+   virtual void SetInputList(TList *input) { fInput = input; }
+   virtual TList *GetOutputList() const { return fOutput; }
+   virtual void SlaveTerminate();
+   virtual void Terminate();
 
-   EvtSelector(TTree * /*tree*/ =0) { }
-   virtual ~EvtSelector() { }
-   virtual Int_t   Version() const { return 2; }
-   virtual void    Begin(TTree *tree);
-   virtual void    SlaveBegin(TTree *tree);
-   virtual void    Init(TTree *tree);
-   virtual Bool_t  Notify();
-   virtual Bool_t  Process(Long64_t entry);
-   virtual Int_t   GetEntry(Long64_t entry, Int_t getall = 0) { return fChain ? fChain->GetTree()->GetEntry(entry, getall) : 0; }
-   virtual void    SetOption(const char *option) { fOption = option; }
-   virtual void    SetObject(TObject *obj) { fObject = obj; }
-   virtual void    SetInputList(TList *input) { fInput = input; }
-   virtual TList  *GetOutputList() const { return fOutput; }
-   virtual void    SlaveTerminate();
-   virtual void    Terminate();
-
-   ClassDef(EvtSelector,0);
-
+   ClassDef(EvtSelector, 0);
 };
 
 #endif
@@ -102,6 +105,5 @@ Bool_t EvtSelector::Notify()
 
    return kTRUE;
 }
-
 
 #endif // #ifdef EvtSelector_cxx
