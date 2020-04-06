703047afec578e743939a8e8dfea41b0ba1954db
Jinnan Zhang <zhangjinnan@ihep.ac.cn>
2020-04-06 10:17:53 +0800

    there

--- Roottest/EnergyLeakage/ELout.cc
+++ Roottest/EnergyLeakage/ELout.cc
@@ -21,8 +21,8 @@ double n_capture = 2.2; //MeV
 double LightYeild = 1200.;
 using namespace std;
 
-double Ran_vis[2] = {0, 13};
-double Ran_true[2] = {0, 13};
+double Ran_x[2] = {0, 17};
+double Ran_y[2] = {1200, 1600};
 int NBinx = 400;
 int NBiny = 400;
 
@@ -31,7 +31,7 @@ int ELout()
     // TChain tE_vis("evt");
     TChain tE_vis("prmtrkdep");
     TChain tE_true("geninfo");
-    for (int nn = 10000; nn < 10999; nn++)
+    for (int nn = 10000; nn < 10001; nn++)
     {
         tE_vis.Add(Form("%s%d.root", HXD, nn));
         tE_true.Add(Form("%s%d.root", HXD, nn));
@@ -44,40 +44,47 @@ int ELout()
     tE_vis.SetBranchStatus("*", 0);
     tE_true.SetBranchStatus("*", 0);
     int nPhotons;
-    float E_dep[2], edepX[2], edepY[2], edepZ[2];
-    // tE_vis.SetBranchStatus("nPhotons", 1);
-    // tE_vis.SetBranchAddress("nPhotons", &nPhotons);
-    tE_vis.SetBranchStatus("edep", 1);
-    tE_vis.SetBranchAddress("edep", &E_dep);
-    tE_vis.SetBranchStatus("edepX", 1);
-    tE_vis.SetBranchAddress("edepX", &edepX);
-    tE_vis.SetBranchStatus("edepY", 1);
-    tE_vis.SetBranchAddress("edepY", &edepY);
-    tE_vis.SetBranchStatus("edepZ", 1);
-    tE_vis.SetBranchAddress("edepZ", &edepZ);
+    // float E_dep[2], edepX[2], edepY[2], edepZ[2];
+    tE_vis.SetBranchStatus("nPhotons", 1);
+    tE_vis.SetBranchAddress("nPhotons", &nPhotons);
+    // tE_vis.SetBranchStatus("edep", 1);
+    // tE_vis.SetBranchAddress("edep", &E_dep);
+    // tE_vis.SetBranchStatus("edepX", 1);
+    // tE_vis.SetBranchAddress("edepX", &edepX);
+    // tE_vis.SetBranchStatus("edepY", 1);
+    // tE_vis.SetBranchAddress("edepY", &edepY);
+    // tE_vis.SetBranchStatus("edepZ", 1);
+    // tE_vis.SetBranchAddress("edepZ", &edepZ);
 
-    int PDGid[2];
-    float Px[2], Py[2], Pz[2];
-    tE_true.SetBranchStatus("InitPDGID", 1);
-    tE_true.SetBranchStatus("InitPX", 1);
-    tE_true.SetBranchAddress("InitPDGID", &PDGid);
-    tE_true.SetBranchAddress("InitPX", &Px);
-    tE_true.SetBranchAddress("InitPZ", &Py);
-    tE_true.SetBranchAddress("InitPY", &Pz);
+    // int PDGid[2];
+    // float Px[2], Py[2], Pz[2];
+    float InitX[2], InitY[2], InitZ[2];
+    // tE_true.SetBranchStatus("InitPDGID", 1);
+    // tE_true.SetBranchStatus("InitPX", 1);
+    tE_true.SetBranchStatus("InitX", 1);
+    tE_true.SetBranchStatus("InitY", 1);
+    tE_true.SetBranchStatus("InitX", 1);
+    // tE_true.SetBranchAddress("InitPDGID", &PDGid);
+    // tE_true.SetBranchAddress("InitPX", &Px);
+    // tE_true.SetBranchAddress("InitPZ", &Py);
+    // tE_true.SetBranchAddress("InitPY", &Pz);
+    tE_true.SetBranchAddress("InitX", &InitX);
+    tE_true.SetBranchAddress("InitZ", &InitY);
+    tE_true.SetBranchAddress("InitY", &InitZ);
 
     double E_true(0), E_vis(0);
-    // TH1D *h_true = new TH1D("E_True", "True Eernergy",NBiny, Ran_vis[0], Ran_vis[1]);
-    // TH1D *h_vis = new TH1D("E_vis", "Visible Eernergy", NBinx, Ran_true[0], Ran_true[1]);
-    // TH2D *h_el = new TH2D("EnergyProfile", "Simulation", NBinx, Ran_true[0], Ran_true[1], NBiny, Ran_vis[0], Ran_vis[1]);
-    // h_el->SetXTitle("True Energy (MeV)");
-    // h_el->SetYTitle("Visible Energy(nPhotons/1200)");
-    // h_el->SetYTitle("Deposited Energy(MeV)");
+    // TH1D *h_true = new TH1D("E_True", "True Eernergy",NBiny, Ran_x[0], Ran_x[1]);
+    // TH1D *h_vis = new TH1D("E_vis", "Visible Eernergy", NBinx, Ran_y[0], Ran_y[1]);
+    TH2D *h_ep = new TH2D("EnergyProfile", "Simulation", NBinx, Ran_x[0], Ran_x[1], NBiny, Ran_y[0], Ran_y[1]);
+    h_ep->SetXTitle("R (m)");
+    h_ep->SetYTitle("nPhotons");
+    // h_ep->SetYTitle("Deposited Energy(MeV)");
     // h_vis->SetXTitle("E (MeV)");
     // TH1D *h_ra = new TH1D("Eratio", "", 400, 0.1, 1.01);
     // h_ra->SetXTitle("E_{dep}/E_{true}");
-    TH2D *h_ra2R = new TH2D("ratio2R", "", NBinx, 0, 18, NBiny, 0, 1.01);
-    h_ra2R->SetYTitle("E_{dep}/E_{true}");
-    h_ra2R->SetXTitle("R (m)");
+    // TH2D *h_ra2R = new TH2D("ratio2R", "", NBinx, 0, 18, NBiny, 0, 1.01);
+    // h_ra2R->SetYTitle("E_{dep}/E_{true}");
+    // h_ra2R->SetXTitle("R (m)");
     double E_ratio(0), R(0);
     double SE_true(0), SE_dep(0);
     int ELnum(0), Tnum(0);
@@ -85,12 +92,13 @@ int ELout()
     {
         tE_vis.GetEntry(i);
         tE_true.GetEntry(i);
-        E_true = TMath::Sqrt(Px[0] * Px[0] + Py[0] * Py[0] + Pz[0] * Pz[0] + M_electron_sq) + M_e;
+        // E_true = TMath::Sqrt(Px[0] * Px[0] + Py[0] * Py[0] + Pz[0] * Pz[0] + M_electron_sq) + M_e;
         // if ( E_dep < E_true)
         // {
-        E_ratio = E_dep[0] / E_true;
-        R = sqrt(edepX[0] * edepX[0] + edepY[0] * edepY[0] + edepZ[0] * edepZ[0]) / 1000.; //to meter
-        h_ra2R->Fill(R,E_ratio);
+        // E_ratio = E_dep[0] / E_true;
+        // R = sqrt(edepX[0] * edepX[0] + edepY[0] * edepY[0] + edepZ[0] * edepZ[0]) / 1000.; //to meter
+        // h_ra2R->Fill(R, E_ratio);
+        R = sqrt(InitX[0] * InitX[0] + InitY[0] * InitY[0] + InitZ[0] * InitZ[0]) / 1000.;
 
         // h_ra->Fill(E_ratio, 1);
         // printf("which: %0.15f\n", E_ratio);
@@ -99,27 +107,27 @@ int ELout()
         // h_true->Fill(E_true);
 
         // E_vis = nPhotons / LightYeild - n_capture;
-        // h_el->Fill(E_true, E_vis);
-        // h_el->Fill(E_true, E_dep);
+        // h_ep->Fill(E_true, E_vis);
+        // h_ep->Fill(E_true, E_dep);
 
         // printf("this entry: %e\n", E_vis);
         // h_vis->Fill(E_vis);
-        if (E_dep[0] < E_true)
-            ELnum++;
-        SE_true += E_true;
-        SE_dep += E_dep[0];
-        Tnum++;
+        // if (E_dep[0] < E_true)
+        //     ELnum++;
+        // SE_true += E_true;
+        // SE_dep += E_dep[0];
+        // Tnum++;
     }
-    printf("Total leakage: %f\n", 1. - SE_dep / SE_true);
-    printf("total Leakage NUM:%f\n", (float)ELnum / Tnum);
+    // printf("Total leakage: %f\n", 1. - SE_dep / SE_true);
+    // printf("total Leakage NUM:%f\n", (float)ELnum / Tnum);
     TFile *ff_EL = TFile::Open("JUNOEnergyLeakage.root", "RECREATE");
     ff_EL->cd();
     // h_true->Write();
     // h_vis->Write();
-    // h_el->Write();
+    h_ep->Write();
     // h_ra->Scale(1 / h_ra->Integral());
     // h_ra->Write();
-    h_ra2R->Write();
+    // h_ra2R->Write();
 
     ff_EL->Close();
 
