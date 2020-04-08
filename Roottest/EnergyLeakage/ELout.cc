28e91bbd7b507ac13a744b26cadae81f67c659ed
Jinnan Zhang <zhangjinnan@ihep.ac.cn>
2020-04-08 09:31:51 +0800

    LY

--- Roottest/EnergyLeakage/ELout.cc
+++ Roottest/EnergyLeakage/ELout.cc
@@ -62,16 +62,16 @@ int ELout()
     float InitPX[2], InitPY[2], InitPZ[2];
     float InitX[2], InitY[2], InitZ[2];
     // tE_true.SetBranchStatus("InitPDGID", 1);
-    // tE_true.SetBranchStatus("InitPX", 1);
-    // tE_true.SetBranchStatus("InitPY", 1);
-    // tE_true.SetBranchStatus("InitPZ", 1);
+    tE_true.SetBranchStatus("InitPX", 1);
+    tE_true.SetBranchStatus("InitPY", 1);
+    tE_true.SetBranchStatus("InitPZ", 1);
     tE_true.SetBranchStatus("InitX", 1);
     tE_true.SetBranchStatus("InitY", 1);
     tE_true.SetBranchStatus("InitZ", 1);
     // tE_true.SetBranchAddress("InitPDGID", &PDGid);
-    // tE_true.SetBranchAddress("InitPX", &InitPX);
-    // tE_true.SetBranchAddress("InitPZ", &InitPY);
-    // tE_true.SetBranchAddress("InitPY", &InitPZ);
+    tE_true.SetBranchAddress("InitPX", &InitPX);
+    tE_true.SetBranchAddress("InitPZ", &InitPY);
+    tE_true.SetBranchAddress("InitPY", &InitPZ);
     tE_true.SetBranchAddress("InitX", &InitX);
     tE_true.SetBranchAddress("InitY", &InitY);
     tE_true.SetBranchAddress("InitZ", &InitZ);
@@ -80,10 +80,10 @@ int ELout()
     // TH1D *h_true = new TH1D("E_True", "True Eernergy",NBiny, Ran_x[0], Ran_x[1]);
 
     //light yield curve
-    // TH1D *h_LY = new TH1D("E_vis", "Visible Eernergy", NBinx, Ran_x[0], Ran_x[1]);
-    // h_LY->SetXTitle("R^{3} (m^{3})");
-    // h_LY->SetYTitle("nPhotons/MeV");
-    // //nPhotons curve
+    TH1D *h_LY = new TH1D("h_LY", "Light Yield Curve", NBinx, Ran_x[0], Ran_x[1]);
+    h_LY->SetXTitle("R^{3} (m^{3})");
+    h_LY->SetYTitle("nPhotons/MeV");
+    //nPhotons curve
     // TH1D *h_nPho = new TH1D("Eratio", "", NBinx, Ran_x[0], Ran_x[1]);
 
     // TH2F *h_ep = new TH2F("EnergyProfile", "Simulation", NBinx, Ran_x[0], Ran_x[1], NBiny, Ran_y[0], Ran_y[1]);
@@ -93,14 +93,14 @@ int ELout()
     // h_ep->SetYTitle("Deposited Energy(MeV)");
 
     // h_nPho->SetXTitle("E_{dep}/E_{true}");
-    TH2D *h_xy = new TH2D("EnergyProfile", "", NBinx, -18, 18, NBinx, -18, 18);
-    h_xy->SetXTitle("x (m)");
-    h_xy->SetYTitle("z (m)");
+    // TH2D *h_xy = new TH2D("EnergyProfile", "", NBinx, -18, 18, NBinx, -18, 18);
+    // h_xy->SetXTitle("x (m)");
+    // h_xy->SetYTitle("z (m)");
     double E_ratio(0), R_cubic(0);
     double Photon2edep(0), Costheta(0);
     double SE_true(0), SE_dep(0);
     // int ELnum(0), Tnum(0);
-    int TotalBIN = h_xy->GetSize();
+    int TotalBIN = h_LY->GetSize();
     int BinArray[TotalBIN];
     float BinValue[TotalBIN];
     for (int i = 0; i < TotalBIN; i++)
@@ -120,13 +120,13 @@ int ELout()
         // R_cubic = sqrt(edepX[0] * edepX[0] + edepY[0] * edepY[0] + edepZ[0] * edepZ[0]) / 1000.; //to meter
         // h_xy->Fill(R_cubic, E_ratio);
         // R_cubic = pow((InitX[0] * InitX[0] + InitY[0] * InitY[0] + InitZ[0] * InitZ[0]), 1.5) / 1e9;
-        // TVector3 EvtPos(InitX[0] / 1e3, InitY[0] / 1e3, InitZ[0] / 1e3);
+        TVector3 EvtPos(InitX[0] / 1e3, InitY[0] / 1e3, InitZ[0] / 1e3);
         Photon2edep = nPhotons / edep;
-        ithBIN = h_xy->Fill(InitX[0]/1e3, InitZ[0]/1e3, Photon2edep);
-        // R_cubic = pow(EvtPos.Mag2(), 1.5);
+        // ithBIN = h_xy->Fill(InitX[0]/1e3, InitZ[0]/1e3, Photon2edep);
+        R_cubic = pow(EvtPos.Mag2(), 1.5);
         // Costheta = EvtPos.CosTheta();
         // h_ep->Fill(R_cubic, Costheta, Photon2edep);
-        // h_ep->Fill(R_cubic, Costheta);
+        ithBIN = h_LY->Fill(R_cubic, Photon2edep);
         // ithBIN = h_ep->Fill(R_cubic, Costheta, Photon2edep);
         // ithBIN = h_ep->FindBin(R_cubic, Costheta);
         // if (ithBIN > 1000)
@@ -157,13 +157,13 @@ int ELout()
     }
     for (int i = 0; i < TotalBIN; i++)
     {
-        SE_dep = h_xy->GetBinContent(i + 1);
+        SE_dep = h_LY->GetBinContent(i + 1);
         // printf("i:%d\tnum:%f\n", i, h_ep->GetBinContent(956));
 
         if (BinArray[i] > 1)
         {
             SE_dep /= BinArray[i]; //average of ith bin
-            h_xy->SetBinContent(i + 1, SE_dep);
+            h_LY->SetBinContent(i + 1, SE_dep);
             // printf("i:%d\tnum:%d\tthese:%f\n", i, BinArray[i], SE_dep);
         }
     }
@@ -172,11 +172,11 @@ int ELout()
     TFile *ff_EL = TFile::Open("JUNOEnergyLeakage.root", "RECREATE");
     ff_EL->cd();
     // h_true->Write();
-    // h_LY->Write();
+    h_LY->Write();
     // h_ep->Write();
     // h_nPho->Scale(1 / h_nPho->Integral());
     // h_nPho->Write();
-    h_xy->Write();
+    // h_xy->Write();
 
     ff_EL->Close();
 
