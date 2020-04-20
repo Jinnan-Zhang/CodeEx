faa7168d5532a92b1f3c2d89ae502e8a9fa9e80e
Jinnan <zhangjinnan@ihep.ac.cn>
2020-04-20 16:25:39 +0800

    path name ok

--- WinShell/MoveCopyReName.bat
+++ WinShell/MoveCopyReName.bat
@@ -1,6 +1,8 @@
 @echo off
-echo please enter mass number y/n:
-set /p Nmass=
-set /a massToday=Nmass+1
+REM echo please enter mass number y/n:
+REM set /p Nmass=
+REM set /a massToday=Nmass+1
 set FromPath=../Template/containerA/
-echo "%FromPath%PMT_Ath.xlsx"
+REM echo "%FromPath%PMT_Ath.xlsx"
+set paths=a c b
+echo %paths%
