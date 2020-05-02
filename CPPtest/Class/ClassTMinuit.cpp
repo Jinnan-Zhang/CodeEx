2c497d1ec074ef3f5bd98abc2c7527738a42ce63
Jinnan Zhang <zhangjinnan@ihep.ac.cn>
2020-05-02 20:15:33 +0800

    inherit is good

--- CPPtest/Class/ClassTMinuit.cpp
+++ CPPtest/Class/ClassTMinuit.cpp
@@ -1,7 +1,7 @@
 //目的：类的成员函数作为TMinuit的函数：
 #include <TMinuit.h>
 #include <TF1.h>
-#include<TH1.h>
+#include <TH1.h>
 
 class TestFCN
 {
@@ -9,7 +9,7 @@ private:
     /* data */
     double a_res = 1.2 / 1e2;
     double b = 0.24 / 1e2;
-    TF1 *f_1;
+
     TMinuit *mMinuit;
 
 public:
@@ -18,20 +18,38 @@ public:
     static double TTF1(double *x, double *par) { return x[0] * x[0]; }
     ~TestFCN();
 };
-
 TestFCN::TestFCN(/* args */)
 {
-    f_1=new TF1("f1",TestFCN::TTF1,-5,5,0);
-    f_1->Draw();
 }
 
 TestFCN::~TestFCN()
 {
 }
 
+class TestFCN1 : public TestFCN
+{
+private:
+    /* data */
+    TF1 *f_1;
+
+public:
+    TestFCN1(/* args */);
+    ~TestFCN1();
+};
+
+TestFCN1::TestFCN1(/* args */)
+{
+    f_1 = new TF1("f1", TestFCN::TTF1, -5, 5, 0);
+    f_1->Draw();
+}
+
+TestFCN1::~TestFCN1()
+{
+}
+
 int ClassTMinuit()
 {
-    TestFCN tf;
+    TestFCN1 tf;
 
     return 0;
 }
