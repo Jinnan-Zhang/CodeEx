8a608a26976bc133302f56e9f1ea19c779cfe163
Jinnan Zhang <zhangjinnan@ihep.ac.cn>
2020-05-14 09:32:37 +0800

    interpolate sometimes faster

---
+++ Roottest/eff/EffTest.cc
@@ -0,0 +1,61 @@
+//purpose: test efficiency of different method
+#include <TH1.h>
+#pragma once
+/*
+usage:
+StartTimeChrono(num);
+...
+StopTimeChrono(num);
+PrintTimeChrono(num,"discription")
+*/
+#include <chrono>
+
+#define StartTimeChrono(_num) \
+    auto start##_num = std::chrono::high_resolution_clock::now()
+
+#define StopTimeChrono(_num) \
+    auto stop##_num = std::chrono::high_resolution_clock::now()
+
+#ifdef _WIN32
+#define PrintTimeChrono(_num, _str)                                                                    \
+    auto dura##_num = std::chrono::duration_cast<std::chrono::microseconds>(stop##_num - start##_num); \
+    printf("Time for calculating %s is %f millisecond.\n", _str, dura##_num.count())
+#else
+#define PrintTimeChrono(_num, _str)                                                                    \
+    auto dura##_num = std::chrono::duration_cast<std::chrono::microseconds>(stop##_num - start##_num); \
+    printf("Time for calculating %s is %f second.\n", _str, dura##_num.count() / 1.e6)
+#endif
+double GFCN(double x)
+{
+    double a = 1. / sqrt(2 * 3.14) / 1. * exp(-0.5 * x * x);
+    double b = 1. / sqrt(2 * 3.14) / 1. * exp(-0.5 * x * x);
+    double c = 1. / sqrt(2 * 3.14) / 1. * exp(-0.5 * x * x);
+    return a + b * c;
+}
+void tHistFCN()
+{
+    TH1D *h_d = new TH1D("h_d", "hh", 200, -1, 1);
+    h_d->FillRandom("gaus");
+    double N = 1e8;
+    double sum = 0;
+    StartTimeChrono(1);
+    for (int i = 0; i < N; i++)
+    {
+        sum += h_d->Interpolate(0.15);
+    }
+    StopTimeChrono(1);
+    PrintTimeChrono(1, "hist");
+    sum = 0;
+    StartTimeChrono(2);
+    for (int i = 0; i < N; i++)
+    {
+        sum += GFCN(0.15);
+    }
+    StopTimeChrono(2);
+    PrintTimeChrono(2, "fcn");
+}
+int EffTest()
+{
+    tHistFCN();
+    return 0;
+}
