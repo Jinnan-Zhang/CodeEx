f14a480186e1cb793141f4d55f2ab5dd403916b7
Jinnan Zhang <zhangjinnan@ihep.ac.cn>
2020-05-23 20:30:05 +0800

    TString somehow like string

--- Roottest/data/Bkg/BkgInter.cpp
+++ Roottest/data/Bkg/BkgInter.cpp
@@ -1,3 +1,14 @@
 // to interpolate before calculation Bkg?
 #include <TFile.h>
 #include <TH1.h>
+#include <TString.h>
+
+void ffcn(TString aname)
+{
+    printf("%s\n", aname.Data());
+}
+int BkgInter()
+{
+    ffcn("aa");
+    return 0;
+}
