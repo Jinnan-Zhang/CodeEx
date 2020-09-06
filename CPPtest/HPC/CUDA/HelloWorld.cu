ef611a2e8e3dd1a702c7345cf391a2fb8400ad18
Jinnan Zhang <zhangjinnan@ihep.ac.cn>
2020-09-06 22:28:12 +0800

    why not better

--- CPPtest/HPC/CUDA/HelloWorld.cu
+++ CPPtest/HPC/CUDA/HelloWorld.cu
@@ -6,7 +6,33 @@
 #include <cuda_runtime.h>
 #include <cuda.h>
 #include <curand.h>
-#define N 64
+#define N 48
+
+#pragma once
+/*
+usage:
+StartTimeChrono(num);
+...
+StopTimeChrono(num);
+PrintTimeChrono(num,"discription")
+*/
+#include <chrono>
+
+#define StartTimeChrono(_num) \
+    auto start##_num = std::chrono::high_resolution_clock::now()
+
+#define StopTimeChrono(_num) \
+    auto stop##_num = std::chrono::high_resolution_clock::now()
+
+#ifdef _WIN32
+#define PrintTimeChrono(_num, _str)                                                                    \
+    auto dura##_num = std::chrono::duration_cast<std::chrono::microseconds>(stop##_num - start##_num); \
+    printf("Time for calculating %s is %f millisecond.\n", _str, dura##_num.count())
+#else
+#define PrintTimeChrono(_num, _str)                                                                    \
+    auto dura##_num = std::chrono::duration_cast<std::chrono::microseconds>(stop##_num - start##_num); \
+    printf("Time for calculating %s is %.8f second.\n", _str, dura##_num.count() / 1.e6)
+#endif
 
 __global__ void helloworld(void)
 {
@@ -50,18 +76,26 @@ void DoVector()
     random_ints(b, N);
     cudaMemcpy(d_a, a, size, cudaMemcpyHostToDevice);
     cudaMemcpy(d_b, b, size, cudaMemcpyHostToDevice);
+    StartTimeChrono(1);
     mul<<<1, N>>>(d_a, d_b, d_sum);
 
+    StopTimeChrono(1);
     cudaMemcpy(&sum, d_sum, sizeof(int), cudaMemcpyDeviceToHost);
+    StartTimeChrono(2);
     int sum_cpu = 0;
     for (int i = 0; i < N; i++)
     {
         // printf("%d\t%d\n", a[i], b[i]);
         sum_cpu += (a[i] * b[i]);
     }
+    StopTimeChrono(2);
     printf(" GPU result:%d\t CPU result:%d\n", sum, sum_cpu);
+    PrintTimeChrono(1, "GPU");
+    PrintTimeChrono(2, "CPU");
     free(a), free(b);
     cudaFree(d_a), cudaFree(d_b), cudaFree(d_sum);
+    cudaDeviceReset();
+
 }
 
 int main(void)
