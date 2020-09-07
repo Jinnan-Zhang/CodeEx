44d306574a7c9de42e26ad554a4007387a2bd4c2
Jinnan Zhang <zhangjinnan@ihep.ac.cn>
2020-09-07 23:48:02 +0800

    still warning

--- CPPtest/HPC/CUDA/HelloWorld.cu
+++ CPPtest/HPC/CUDA/HelloWorld.cu
@@ -6,35 +6,12 @@
 #include <cuda_runtime.h>
 #include <cuda.h>
 #include <curand.h>
+#include <HelloWorld.cuh>
 #define N (2048 * 2048)
 #define THREAD_PER_BLOCK 512
 #define BLOCKS_NUM (N / THREAD_PER_BLOCK)
 
-#pragma once
-/*
-usage:
-StartTimeChrono(num);
-...
-StopTimeChrono(num);
-PrintTimeChrono(num,"discription")
-*/
-#include <chrono>
 
-#define StartTimeChrono(_num) \
-    auto start##_num = std::chrono::high_resolution_clock::now()
-
-#define StopTimeChrono(_num) \
-    auto stop##_num = std::chrono::high_resolution_clock::now()
-
-#ifdef _WIN32
-#define PrintTimeChrono(_num, _str)                                                                    \
-    auto dura##_num = std::chrono::duration_cast<std::chrono::microseconds>(stop##_num - start##_num); \
-    printf("Time for calculating %s is %f millisecond.\n", _str, dura##_num.count())
-#else
-#define PrintTimeChrono(_num, _str)                                                                    \
-    auto dura##_num = std::chrono::duration_cast<std::chrono::microseconds>(stop##_num - start##_num); \
-    printf("Time for calculating %s is %.8f second.\n", _str, dura##_num.count() / 1.e6)
-#endif
 
 __global__ void helloworld(void)
 {
