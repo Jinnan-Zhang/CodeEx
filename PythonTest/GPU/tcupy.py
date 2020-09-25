2339ac49c2691c526817f651afbeb3b74dcb24e9
Jinnan Zhang <zhangjinnan@ihep.ac.cn>
2020-09-25 16:57:01 +0800

    on cupy

---
+++ PythonTest/GPU/tcupy.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+import cupy as cp
+import numpy as np
+import ROOT
+
+def MatMultiply(filename="../plots/Contour/CosProfile.txt"):
+    data=
+
+if __name__ == "__main__":
+    MatMultiply()
