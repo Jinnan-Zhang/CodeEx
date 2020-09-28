ddbeaaa381f38240ed32d75680deefdae2d29529
Jinnan Zhang <zhangjinnan@ihep.ac.cn>
2020-09-28 19:28:02 +0800

    set

--- PythonTest/GPU/tcupy.py
+++ PythonTest/GPU/tcupy.py
@@ -4,12 +4,13 @@ import cupy as cp
 import numpy as np
 import ROOT
 
+
 def MatMultiply(filename="../plots/Contour/DataProfile.txt"):
-    data=np.loadtxt(filename)
+    data = np.loadtxt(filename)
     print(data.size)
-    R_x=data[:,0].reshape(50,50)
-    Cos_x=data[:,1].reshape(50,50)
-    nPE_y=data[:,2].reshape(50,50)
+    R_x = data[:, 0].reshape(50, 50)
+    Cos_x = data[:, 1].reshape(50, 50)
+    nPE_y = data[:, 2].reshape(50, 50)
 
 
 if __name__ == "__main__":
