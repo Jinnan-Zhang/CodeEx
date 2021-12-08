a8c2ad8f98ec65a6d4d2988b985a6dace64a717a
Jinnan Zhang <zhangjinnan@ihep.ac.cn>
2021-12-08 14:20:07 +0800

    thread safety fixed the problem

--- CPPtest/HPC/IntOpenMP.cpp
+++ CPPtest/HPC/IntOpenMP.cpp
@@ -1,12 +1,14 @@
 /*
-* @Compile comment g++ -I `root-config --incdir` -o IntOpenMP.cpp.o IntOpenMP.cpp -fopenmp `root-config --libs` 
-*/
+ * @Compile comment:
+ g++ -I `root-config --incdir` -o IntOpenMP.cpp.o IntOpenMP.cpp -g -fopenmp `root-config --libs`
+ */
 #include <TF1.h>
 #include <omp.h>
 #include <TH1.h>
+#include <TROOT.h>
 const double x_low = 0;
 const double x_up = 10;
-void TestIntgral(int BinNUM = 10);
+void TestIntgral(int BinNUM = 1200);
 
 int main()
 {
@@ -25,16 +27,23 @@ void TestIntgral(int BinNUM)
     h_1->GetLowEdge(Edges);
     Edges[N] = x_up;
     double w = h_1->GetBinWidth(1);
+    // TF1 *f_t[BinNUM];
+    // for (int i = 0; i < BinNUM; i++)
+        // f_t[i] = new TF1(Form("f_s%d", i), "x^2-1", x_low, x_up);
+    // Make ROOT thread-safe
+    ROOT::EnableThreadSafety();
 #pragma omp parallel for
     for (int i = 0; i < BinNUM; i++)
     {
-        TF1 *f_t = new TF1(Form("f_s%d", i), "x^2-1", x_low, x_up);
-        // (TF1*)f_0->Clone(Form("f_s%d",i));
-        a_data[i] = f_t->Integral(Edges[i], Edges[i + 1]);
+        // TF1 *f_t = new TF1(Form("f_s%d", i), "x^2-1", x_low, x_up);
+        double err = 0;
+        // TF1 *f_t=(TF1*)f_0->Clone(Form("f_s_%d",i));
+        // a_data[i] = f_t->Integral(Edges[i], Edges[i + 1]);
+        a_data[i] = f_0->Integral(Edges[i], Edges[i + 1]);
         // a_data[i] = f_t->Eval(Edges[i]);
         // a_data[i]=Edges[i]+ Edges[i + 1]*10;
-        printf("a: %f\t b: %f\t v: %f\n", Edges[i], Edges[i + 1], a_data[i]);
-        delete f_t;
+        printf("i: %d\t a: %f\t b: %f\t v: %f\n", i, Edges[i], Edges[i + 1], a_data[i]);
+        // delete f_t;
     }
     // for (int i = 0; i < BinNUM; i++)
     //     printf("a: %f\t b: %f\t v: %f\n", Edges[i], Edges[i + 1], a_data[i]);
